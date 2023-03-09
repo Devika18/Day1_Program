56719ced4c4f7d466c7bf88780f35d104a2efe8c
devikakarpe <devikankarpe@gmail.com>
2023-03-09 15:19:26 +0530

    added logic for checking leap year

---
+++ src/LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String args[]) {
+        System.out.println("Enter the Year:");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if (year >= 1584) {
+            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))) {
+                System.out.println("It is a Leap Year");
+            } else {
+                System.out.println("It is not a leap Year");
+            }
+        } else {
+            System.out.println("Please enter the valid year");
+        }
+    }
+}
