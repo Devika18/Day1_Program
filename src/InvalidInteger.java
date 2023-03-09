d23138c7544676bae657e179eb34acc74c2e9101
devikakarpe <devikankarpe@gmail.com>
2023-03-09 15:05:38 +0530

    added logic for invalid integers

---
+++ src/InvalidInteger.java
@@ -0,0 +1,16 @@
+public class InvalidInteger {
+    public static void main (String[] args) {
+        int num,count=0, sum=0;
+        for (int i=0; i<args.length; i++) {
+            try {
+                num = Integer.parseInt(args[i]);
+                sum = sum + num;
+            } catch (NumberFormatException e) {
+                count++;
+            }
+
+        }
+        System.out.println("The sum of valid integer is: " + sum);
+        System.out.println("Invalid integers are: "+ count);
+    }
+}
