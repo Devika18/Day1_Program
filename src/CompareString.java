b7422db50528b5d26a8ba769a80a34806981e180
devikakarpe <devikankarpe@gmail.com>
2023-03-09 14:50:49 +0530

    added logic for comparing two strings

---
+++ src/CompareString.java
@@ -0,0 +1,12 @@
+public class CompareString {
+    public static void main (String[] args) {
+        String str1 = "Devika";
+        String str2 = "Devika";
+        boolean r = str1.equals(str2);
+        if (str1 == str2) {
+            System.out.println("Both strings are equal ");
+        }
+        else
+            System.out.println("Strings are not Equal");
+    }
+}
