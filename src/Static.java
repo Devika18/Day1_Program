49838e3529cd0cfbd4385aa441739ab80dc17203
devikakarpe <devikankarpe@gmail.com>
2023-03-09 15:11:55 +0530

    added logic for demonstrating static variable, method and block

---
+++ src/Static.java
@@ -0,0 +1,18 @@
+public class Static {
+    //This is static variables
+    static int n;
+    static  String s;
+
+    //This is a static block
+    static
+    {
+        n = 18;
+        s = "I Am Devika";
+    }
+
+    //This is static method
+    public static void main(String[] args) {
+        System.out.println("Number is: "+ n);
+        System.out.println("String is: "+ s);
+    }
+}
